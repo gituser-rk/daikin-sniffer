8a0d90bbdbb17f950f8a4ece6d1fe1e941c740ee
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2026-02-19 22:17:16 +0100

    Update component.py

--- esphome/components/daikin_sniffer/component.py
+++ esphome/components/daikin_sniffer/component.py
@@ -1,17 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_UART_ID
 
 daikin_ns = cg.esphome_ns.namespace("daikin_sniffer")
 DaikinSniffer = daikin_ns.class_("DaikinSniffer", cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(DaikinSniffer),
-}).extend(uart.UART_DEVICE_SCHEMA)
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+})
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config["uart_id"])
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID], uart_component)
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
