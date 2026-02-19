0fedc9a65445db2fa963361f06df0b844c24a49c
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2026-02-19 16:20:46 +0100

    Update daikin_sniffer.h

--- components/daikin_sniffer/daikin_sniffer.h
+++ components/daikin_sniffer/daikin_sniffer.h
@@ -4,13 +4,13 @@
 
 class DaikinSniffer : public esphome::Component, public esphome::uart::UARTDevice {
  public:
-  esphome::text_sensor::TextSensor *sensor = new esphome::text_sensor::TextSensor();
+  esphome::text_sensor::TextSensor *sensor;
 
   DaikinSniffer(esphome::uart::UARTComponent *parent);
 
+  void setup() override;
   void loop() override;
 
  private:
   std::vector<uint8_t> buffer;
 };
-
